257511d360d80e2514860d5d37f0f771a329ad84
Ravipendyala <ravipendyala@hotmail.com>
2020-06-26 20:27:14 +0530

    Delete main.dart

--- main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'CustomWidget.dart';
-import 'OnTheFlyForm.dart';
-
-//void main() => runApp(CustomWidgetExample());
-void main() => runApp(DynamicForm());
